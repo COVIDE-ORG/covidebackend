0d86cc5fb0abceb6e6a2843f8ad43133273902a1
SAMBIT MAJHI <majhisambit2@gmail.com>
2021-05-07 20:48:45 +0530

    Revert "Merge pull request #3 from SaatwikRishi/feature-oxygen"
    This reverts commit 2500709e8fc03bcdd454e42ae5a6b9327aa68eba, reversing
    changes made to d688c8e34fa1cc47eab49dc3c6315c6ad63befa7.

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-
-
-const { response } = require("express");
 
 //Import Routes
 
@@ -19,28 +15,9 @@ app.use(cookieParser());
 
 //Start a server
 app.listen(process.env.DEV_PORT, () => {
-    console.log("Server Running!");
+  console.log("Server Running!");
 });
-
-
-const func = require("./functions");
-
-func.fetchallDataFromSheet()
-
-const vd = require('./variableData');
 
 app.get("/", (req, res) => {
-    res.send("COVID BACKEND");
-});
-
-
-app.get("/getplasma", (req, res) => {
-    res.send(vd.getdata().plasma)
-
-});
-
-
-app.get("/getoxygen", (req, res) => {
-    res.send(vd.getdata().oxygen)
-
+  res.send("Covid Backend!!");
 });
